f435607d187d85d747040e4d6d89091026dd607e
Apsernabanu1234 <144452900+Apsernabanu1234@users.noreply.github.com>
2023-10-29 11:21:18 +0530

    Add files via upload

---
+++ challenge 2.py
@@ -0,0 +1,9 @@
+Implement a recursive function to calculate the factorial of a given number
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
+
+num = 5
+print("Factorial of", num, "is", factorial(num))
